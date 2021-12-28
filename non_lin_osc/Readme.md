f677d0d747b2c7186e0ad62ff8e35ff35fb24076
Vladislav Zavjalov <slazav@altlinux.org>
2021-12-28 02:21:51 +0000

    non_lin_osc: osc_solve_vdp_* functions

--- non_lin_osc/Readme.md
+++ non_lin_osc/Readme.md
@@ -4,9 +4,38 @@
 x'' + func(x,x',pars) = F*cos(w*t)
 ```
 
+#### Examples of oscillators
+
+* harmonic osc, `osc_harm(x,dx, (w0,tau))`
+
+Parameters: 
+- `w0` -- resonant frequency
+- `tau` -- relaxation time
+
+Formula: `func(x,v) = w0**2*x + v/tau`
+
+* duffing osc, `osc_duffing(x,dx, (w0,tau,a))`
+
+- `w0`  - resonant frequency
+- `tau` - relaxation time at low drive
+- `a`   - non-linear parameter
+
+Formula: `func(x,v) = w0**2*x + v/tau + a*x**3`
+
+* pseudoplastic osc N1, `osc_pseudopl1(x,dx, (w0,tau,vc,k))`
+
+Formula: `func(x,v) = w0**2*x + v/tau * (k - (1-k) * vc/numpy.sqrt(vc**2 + v**2))`
+
+* pseudoplastic osc N2, `osc_pseudopl2(x,dx, (w0,tau,vc))`
+
+Formula: `func(x,v) = w0**2*x + v/tau * vc/numpy.sqrt(vc**2 + v**2)`
+
+
+
 #### Solving DE with periodic BC
 
 This is the most general approach for finding response of a non-linear oscillator.
+It is slow and in some cases not accurate.
 
 * `res = osc_solve_per_func(func, pars, F, w)`
 
@@ -25,4 +54,40 @@ Calculate N-th harmonic of a function returned by osc_solve_per().
 Calculate response of a non-linear oscillator on the driving frequency.
 It's just a combination of osc_solve_per_func() and osc_solve_per_harm() calls.
 
+
+#### Averaging motion in Van-der-Pol coordinates
+
+This is recommended way of solving non-linear oscillators. The idea is to
+use van der Pol coordinates (u,v) = (x, x'/w rotated by w*t), and then
+numerically average equations over rotation period (phase 0..2*pi) And
+then find equilibrium, (u',v')=0.
+
+See texts:
+- http://www.scholarpedia.org/article/Duffing_oscillator
+- http://www.scholarpedia.org/article/Equilibrium
+
+* `osc_solve_vdp_eq(uv, func, pars, F, w)`
+
+Find (\dot u,\dot v) as a function of (u,v) for period-averaged motion of
+the periodically-driven non-linear oscillator.
+
+* `osc_solve_vdp(func, pars, F, w, a0=0, p0=0)`
+
+Find equilibrium (zero of nonlin_osc_eq function).
+This is enough for simple small non-linearities,
+for duffing oscillator it does not work properly
+(one should integrate trajectories in u-v space instead)
+
+#### Duffing oscillator
+
+Formulas for Duffing oscillator. They should be fully equivalent to `osc_solve_vdp_*`,
+but faster.
+
+* `osc_solve_vdp_duff_eq(uv, w0, tau, a, F, w)` -- Equivalent of `osc_solve_vdp_eq`.
+
+* `osc_solve_vdp_duff(w0, tau, a, F, w, a0=0, p0=0)` -- Equivalent of `osc_solve_vdp`
+
+* `osc_duff_amp(w,F,w0,tau, a)` -- Analytical expression for amplitude of Duffing oscillator.
+(Solve qubic equation `((w**2 - w0**2 - 3/4.0*a * A**2)**2 + (w/tau)**2)*A**2 = F**2`,
+return only real roots.)
 
--- non_lin_osc/non_lin_osc.py
+++ non_lin_osc/non_lin_osc.py
@@ -7,104 +7,24 @@ from scipy.integrate import solve_bvp,trapz,quad
 
 #######################################################
 #######################################################
-
-############################
-# Solve ODE of a non-linear oscillator driven by periodic force:
-#   x'' + func(x, x', pars) = F*cos(w*t)
-# on one period (2pi/w) with periodic BC, x(0)=x(T), x'(0)=x'(T).
-# Returns full solution as a cubic spline (see solve_bvp()).
-# Arguments:
-#   func - function
-#   pars - parameters for the function
-#   F - drive amplitude
-#   w - drive frequency
-# Return:
-#   result from solve_bvp()
+# Here we are working with a non-linear oscillator
+# driven by periodic force:
+#     x'' + func(x, x', pars) = F*cos(w*t)
 #
-def osc_solve_per_func(func, pars, F, w):
-
-  def bc(Xa, Xb):
-    return numpy.array([Xa[0]-Xb[0], Xa[1]-Xb[1]])
-
-  def rhs_func(t, X):
-    RHS = -func(X[0], X[1], pars) - F*numpy.cos(w*t)
-    return numpy.vstack((X[1], RHS))
-
-  # initial mesh:
-  t0 = numpy.linspace(0, 2*math.pi/w, 5)
-  x0 = numpy.zeros((2, t0.size))
-  return solve_bvp(rhs_func, bc, t0, x0)
-
-############################
-# Calculate N-th harmonic of a function returned by
-# osc_solve_per()
-#
-def osc_solve_per_harm(res, N):
-  T=res.x[-1]
-  W=2*math.pi/T * N
-  fx = lambda t: numpy.cos(W*t)*res.sol(t)[0]
-  fy = lambda t: numpy.sin(W*t)*res.sol(t)[0]
-  x = quad(fx, 0, T, limit=200)[0]
-  y = quad(fy, 0, T, limit=200)[0]
-  return (2*x/T, 2*y/T)
-
-############################
-# Calculate response of a non-linear oscillator on the driving frequency.
-# Combination of osc_solve_per_func() and osc_solve_per_harm() calls.
-#
-def osc_solve_per(func, pars, F, w):
-  res = osc_solve_per_func(func, pars, F, w)
-  return osc_solve_per_harm(res,1)
-
-
-#######################################################
-#######################################################
-
-# Equilibriun function of a non-linear oscillator
-# at periodic drive.
-#
-# u,v - van der Pol coordinates (x,x' rotated by w*t)
-# w - frequency
-# func - second derivative function, x'' = -func(x, x')
-#
-# Coordinated are averaged over rotation period (phase 0..2*pi)
-# Output is u' and v',  they should be zero in equilibrium.
-#
-# See text in http://www.scholarpedia.org/article/Duffing_oscillator
-#
-
-#######################################################
-# Find (\dot u,\dot v) as a function of (u,v) for period-averaged motion of
-# a periodically-driven non-linear oscillator x'' + func(x,x') = F*cos(w*t)
-def nonlin_osc_eq(uv,w, F, func, fpars):
-  p = 2*math.pi*numpy.linspace(0,1,100) # phase for integration
-  sp = numpy.sin(p)
-  cp = numpy.cos(p)
-
-  x = uv[0]*cp-uv[1]*sp;
-  y = w*(-uv[0]*sp-uv[1]*cp);
-  z = (F*cp - func(x,y,fpars) + w**2*x)/w;
-
-  return [-trapz(z*sp, p),\
-          -trapz(z*cp, p)];
-
-#######################################################
-
-# Find equilibrium (zero of nonlin_osc_eq function).
-# This is enough for simple small non-linearities,
-# for duffing oscillator it does not work properly
-# (one should integrate trajectories in u-v space instead)
-
-def nonlin_osc_solve0(w, F, func, fpars, uv0):
-  return scipy.optimize.fsolve(nonlin_osc_eq, uv0, (w,F,func,fpars))
-
-#######################################################
+# Examples of oscillatiors:
 
 ## harmonic osc
 def osc_harm(x,dx, pars):
   w0  = pars[0] # resonant frequency
-  tau = pars[1] # relaxation time at low velocuty
+  tau = pars[1] # relaxation time
   return w0**2*x + dx/tau
+
+## duffing osc
+def osc_duffing(x,dx, pars):
+  w0  = pars[0] # resonant frequency
+  tau = pars[1] # relaxation time at low drive
+  a   = pars[2] # non-linear parameter
+  return w0**2*x + dx/tau + a*x**3
 
 ## pseudoplastic osc N1
 def osc_pseudopl1(x,dx, pars):
@@ -121,21 +41,150 @@ def osc_pseudopl2(x,dx, pars):
   vc  = pars[2] # critical velocity
   return w0**2*x + dx/tau * vc/numpy.sqrt(vc**2 + dx**2)
 
-## duffing osc
-def osc_duffing(x,dx, pars):
-  w0  = pars[0] # resonant frequency
-  tau = pars[1] # relaxation time at low velocuty
-  a   = pars[2] # non-linear parameter
-  return w0**2*x + dx/tau + a*x**3
 
+############################
+# Solve ODE of the non-linear oscillator driven by periodic force
+# on one period (2pi/w) with periodic BC, x(0)=x(T), x'(0)=x'(T).
+# Returns full solution as a cubic spline (see solve_bvp() return value).
+# Arguments:
+#   func - function
+#   pars - parameters for the function
+#   F - drive amplitude
+#   w - drive frequency
+# Return:
+#   result from solve_bvp()
+#
+def osc_solve_per_func(func, pars, F, w, a0=0, p0=0):
+
+  def bc(Xa, Xb):
+    return numpy.array([Xa[0]-Xb[0], Xa[1]-Xb[1]])
+
+  # Two equations: x' = v; v' = RHS(x,v)
+  def rhs_func(t, X):
+    RHS = -func(X[0], X[1], pars) - F*numpy.cos(w*t)
+    return numpy.vstack((X[1], RHS))
+
+  # initial mesh:
+  t0 = numpy.linspace(0, 2*numpy.pi/w, 50)
+
+  # initial guess
+  x0 = numpy.zeros((2, t0.size))
+  if a0 != 0:
+    x0[0,:] =  a0*numpy.cos(w*t0+p0)
+    x0[1,:] = -a0*w*numpy.sin(w*t0+p0)
+
+  return solve_bvp(rhs_func, bc, t0, x0, tol=1e-4, max_nodes=10000)
+
+############################
+# Calculate N-th harmonic of a function returned by
+# osc_solve_per()
+#
+def osc_solve_per_harm(res, N):
+  if res.status>0: return (numpy.nan,numpy.nan)
+  T=res.x[-1]
+  W=2*math.pi/T*N
+
+#  fx = numpy.cos(W*res.x)*res.y[0]
+#  fy = numpy.sin(W*res.x)*res.y[0]
+#  print('2>', res.x.size, fy[0], fy[-1])
+#  x = trapz(fx, res.x)
+#  y = trapz(fy, res.x)
+
+  fx = lambda t: numpy.cos(W*t)*res.sol(t)[0]
+  fy = lambda t: numpy.sin(W*t)*res.sol(t)[0]
+  x = quad(fx, 0, T, limit=200)[0]
+  y = quad(fy, 0, T, limit=200)[0]
+  return (2*x/T, 2*y/T)
+
+
+############################
+# Calculate response of a non-linear oscillator on the driving frequency.
+# Combination of osc_solve_per_func() and osc_solve_per_harm() calls.
+#
+def osc_solve_per(func, pars, F, w, a0=0, p0=0):
+  res = osc_solve_per_func(func, pars, F, w, a0, p0)
+  return osc_solve_per_harm(res,1)
+
+
+#######################################################
+#######################################################
+
+# Equilibriun function of a non-linear oscillator
+# at periodic drive.
+#
+# u,v - van der Pol coordinates (x,x'/w rotated by w*t)
+# w - frequency
+# func - second derivative function, x'' = -func(x, x')
+#
+# Coordinated are averaged over rotation period (phase 0..2*pi)
+# Output is u' and v',  they should be zero in equilibrium.
+#
+# See text in http://www.scholarpedia.org/article/Duffing_oscillator
+#
+
+#######################################################
+# Find (\dot u,\dot v) as a function of (u,v) for period-averaged motion of
+# a periodically-driven non-linear oscillator x'' + func(x,x') = F*cos(w*t)
+#
+def osc_solve_vdp_eq(uv, func, pars, F, w):
+  p = 2*math.pi*numpy.linspace(0,1,100) # phase for integration
+  sp = numpy.sin(p)
+  cp = numpy.cos(p)
+
+  x = uv[0]*cp-uv[1]*sp;
+  y = w*(-uv[0]*sp-uv[1]*cp);
+  z = (F*cp - func(x,y,pars) + w**2*x)/w;
+
+  return [-trapz(z*sp, p), -trapz(z*cp, p)];
+
+#######################################################
+
+# Find equilibrium (zero of nonlin_osc_eq function).
+# This is enough for simple small non-linearities,
+# for duffing oscillator it does not work properly
+# (one should integrate trajectories in u-v space instead)
+#
+def osc_solve_vdp(func, pars, F, w, a0=0, p0=0):
+  uv0 = [a0*numpy.cos(p0), a0*numpy.sin(p0)]
+  (uv0, info, stat, msg) = scipy.optimize.fsolve(osc_solve_vdp_eq, uv0, (func,pars,F,w,), full_output=True)
+  if stat!=1: return (numpy.nan, numpy.nan)
+
+  # Equilibrium is unstable if Jacobian
+  # has an eigenvalue with positive real part.
+  # or if (trJ<0 || detJ>0)
+  # see https://stackoverflow.com/questions/52309635/check-the-stability-of-differential-equation-using-fsolve
+  #     http://www.scholarpedia.org/article/Equilibrium
+  # does not work yet?
+#  R = numpy.zeros((2,2))
+#  R[numpy.triu_indices(2)] = info["r"]
+#  J = (info["fjac"].T).dot(R)
+#  ee = numpy.real(numpy.linalg.eigvals(J))
+#  if ee[0]>0 or ee[1]>0: return (numpy.nan, numpy.nan)
+
+  return uv0
+
+#######################################################
 #######################################################
 # Equilibriun function and solver for Duffung oscillator at periodic drive.
 # see http://www.scholarpedia.org/article/Duffing_oscillator
 # Note that solver does not work when hysteresis appears.
 
-def nonlin_osc_eq_duff(uv, w, F, w0, tau, a):
+def osc_solve_vdp_duff_eq(uv, w0, tau, a, F, w):
   return [(-w*uv[0]/tau + (w**2-w0**2)*uv[1] - 3/4.0*a*(uv[1]**2+uv[0]**2)*uv[1])/2/w,\
           (-w*uv[1]/tau - (w**2-w0**2)*uv[0] + 3/4.0*a*(uv[1]**2+uv[0]**2)*uv[0] - F)/2/w]
 
-def nonlin_osc_solve_duff(w, F, w0, tau, a, uv0):
-  return scipy.optimize.fsolve(nonlin_osc_eq_duff, uv0, (w,F,w0, tau, a))
+def osc_solve_vdp_duff(w0, tau, a, F, w, a0=0, p0=0):
+  uv0 = [a0*numpy.cos(p0), a0*numpy.sin(p0)]
+  (uv0, info, stat, msg) = scipy.optimize.fsolve(osc_solve_vdp_duff_eq, uv0, (w0, tau, a, F, w), full_output=True)
+  if stat!=1: return (numpy.nan, numpy.nan)
+  return uv0
+
+# Analytical expression for amplitude of Duffing oscillator.
+#  ((w**2 - w0**2 - 3/4.0*a * A**2)**2 + (w/tau)**2)*A**2 = F**2
+# This is a qubic equation for A**2.
+# Returns up to 3 numbers
+#
+def osc_duff_amp(w,F,w0,tau, a):
+  p = [9/16.0*a**2,  - 3/2.0*(w**2-w0**2)*a, (w**2-w0**2)**2 + (w/tau)**2, -F**2]
+  rr = numpy.sqrt(numpy.roots(p))
+  return numpy.real(rr[numpy.imag(rr)==0])
